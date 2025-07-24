d223879a85b86d028be97423e577ce393d60deba
Kamil Porembiński <kamil@thecamels.org>
2025-07-24 10:34:25 +0200

    fix: remove unused bar

--- web/src/components/furniture/nav.tsx
+++ web/src/components/furniture/nav.tsx
@@ -33,11 +33,6 @@ export default component$(() => {
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
       <div class="navbar bg-base-100">
         <div class="flex-1">
-          <div class="flex-none md:hidden">
-            <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-            </label>
-          </div> 
           <a href="/" class="btn btn-ghost text-xl flex capitalize">
             <label for="my-drawer-3" aria-label="open sidebar" class="tooltip tooltip-bottom" data-tip="Zobacz wszystkie strony"><Icon class="mr-2" icon="shield" width={28} height={28}  /></label>
             <h1>Zabezpiecz Się</h1>
