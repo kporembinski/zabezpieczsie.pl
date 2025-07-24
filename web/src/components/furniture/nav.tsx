8c2af7eda9cbab905505563f279412a877cdd12d
Kamil Porembiński <kamil@thecamels.org>
2025-07-24 11:09:06 +0200

    fix: testing

--- web/src/components/furniture/nav.tsx
+++ web/src/components/furniture/nav.tsx
@@ -30,61 +30,61 @@ export default component$(() => {
   return (
     <>
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
-            <div class="navbar bg-base-100 max-w-full overflow-x-hidden">
-        <div class="flex items-center justify-between w-full">
-          <a href="/" class="btn btn-ghost text-xl flex capitalize items-center">
-            <Icon class="mr-2" icon="shield" width={28} height={28}  />
+       <div class="navbar bg-base-100 max-w-full overflow-x-hidden">
+        <div class="flex-1">
+          <a href="/" class="btn btn-ghost text-xl flex capitalize">
+            <label for="my-drawer-3" aria-label="open sidebar" class="tooltip tooltip-bottom" data-tip="Strona główna"><Icon class="mr-2" icon="shield" width={28} height={28}  /></label>
             <h1>Zabezpiecz Się</h1>
           </a>
-
-          <div class="hidden md:flex items-center space-x-4">
-            <ul class="menu menu-horizontal px-1">
-              <li>
-                <details>
-                  <summary class="flex items-center">
-                    <Icon icon="checklist" width={16} height={16}  />
-                    Checklisty
-                  </summary>
-                  <ul class="p-2 bg-base-100 rounded-t-none z-10">
-                    {data.value.map((item: Section, index: number) => (
-                      <li key={`checklist-nav-${index}`} class={`hover:bg-${item.color}-600 hover:bg-opacity-15`}>
-                        <a href={`/checklist/${item.slug}`} class="flex items-center">
-                          <Icon color={item.color} class="mr-2" icon={item.icon} width={16} height={16}  />
-                          {item.title}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a href="https://github.com/kporembinski/personal-security-checklist"
-                   class="tooltip flex tooltip-bottom items-center" data-tip="Zobacz/edycja źródła i danych">
-                  <Icon icon="github" width={16} height={16}  />GitHub
-                </a>
-              </li>
-            </ul>
-            <div class="tooltip tooltip-bottom" data-tip="Motyw">
-              <label class="cursor-pointer grid place-items-center">
-                <input
-                  type="checkbox"
-                  checked={theme.theme === 'dark'}
-                  onClick$={() => {
-                    setTheme(theme.theme === 'dark' ? 'light' : 'dark');
-                  }}
-                  class="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
-                />
-                <svg class="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
-                <svg class="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-              </label>
-            </div>
-            <button
-              onClick$={() => ((document.getElementById('settings_modal') || {}) as HTMLDialogElement).showModal()}
-              class="cursor-pointer tooltip flex tooltip-bottom items-center"
-              data-tip="Ustawienia">
-              <Icon icon="settings" width={20} height={20}  />
-            </button>
+        </div>
+        <div class="flex-none hidden md:flex">
+          <ul class="menu menu-horizontal px-1">
+            <li>
+              <details>
+                <summary>
+                  <Icon icon="checklist" width={16} height={16}  />
+                  Checklisty
+                </summary>
+                <ul class="p-2 bg-base-100 rounded-t-none z-10">
+                  {data.value.map((item: Section, index: number) => (
+                    <li key={`checklist-nav-${index}`} class={`hover:bg-${item.color}-600 hover:bg-opacity-15`}>
+                      <a href={`/checklist/${item.slug}`}>
+                      <Icon color={item.color} class="mr-2" icon={item.icon} width={16} height={16}  />
+                        {item.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a href="https://github.com/kporembinski/personal-security-checklist"
+                class="tooltip flex tooltip-bottom" data-tip="Zobacz/edycja źródła i danych">
+                <Icon icon="github" width={16} height={16}  />GitHub
+              </a>
+            </li>
+          </ul>
+          <div class="tooltip tooltip-bottom" data-tip="Motyw">
+            <label class="cursor-pointer grid place-items-center">
+              <input
+                type="checkbox"
+                checked={theme.theme === 'dark'}
+                onClick$={() => {
+                  setTheme(theme.theme === 'dark' ? 'light' : 'dark');
+                }}
+                class="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+              />
+              <svg class="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+              <svg class="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+            </label>
           </div>
+          <li class="list-none px-2">
+            <p
+              onClick$={() => ((document.getElementById('settings_modal') || {}) as HTMLDialogElement).showModal()}
+              class="cursor-pointer tooltip flex tooltip-bottom" data-tip="Ustawienia">
+                <Icon icon="settings" width={20} height={20}  />
+            </p>
+          </li>
         </div>
       </div>
 
