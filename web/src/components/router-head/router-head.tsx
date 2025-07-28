276fc05c157e06c30c89c618f5c7e272ec3b6e01
Kamil Porembiński <kamil@thecamels.org>
2025-07-28 11:28:20 +0200

    fix: add missing icons

--- web/src/components/router-head/router-head.tsx
+++ web/src/components/router-head/router-head.tsx
@@ -15,9 +15,13 @@ export const RouterHead = component$(() => {
       {/* Site config */}
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/png" href="/favicon.png" />
-      <link rel="apple-touch-icon" href="/favicon.png" />
-      <meta name="theme-color" content="#6419e6" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <meta name="theme-color" content="#0047ff" />
       <link rel="manifest" href="/manifest.json" />
 
       {/* Open Graph / Facebook */}
