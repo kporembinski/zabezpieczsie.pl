75519d291931af60e3a0f79bd6f057ea9a5b417b
Kamil Porembiński <kamil@thecamels.org>
2026-08-25 21:57:24 +0200

    fix: add local loading filel

--- web/src/routes/layout.tsx
+++ web/src/routes/layout.tsx
@@ -1,18 +1,15 @@
 import { component$, useContextProvider, Slot } from "@builder.io/qwik";
 import { routeLoader$, type RequestHandler } from "@builder.io/qwik-city";
 import jsyaml from "js-yaml";
+import checklistYaml from "../../../personal-security-checklist.yml?raw";
 
 import Navbar from "~/components/furniture/nav";
 import Footer from "~/components/furniture/footer";
 import { ChecklistContext } from "~/store/checklist-context";
 import type { Sections } from "~/types/PSC";
 
-export const useChecklists = routeLoader$(async () => {
-  const remoteUrl = 'https://raw.githubusercontent.com/kporembinski/personal-security-checklist/refs/heads/master/personal-security-checklist.yml';
-  return fetch(remoteUrl)
-    .then((res) => res.text())
-    .then((res) => jsyaml.load(res) as Sections)
-    .catch(() => []);
+export const useChecklists = routeLoader$((): Sections => {
+  return jsyaml.load(checklistYaml) as Sections;
 });
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
